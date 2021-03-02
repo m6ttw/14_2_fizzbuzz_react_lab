05ec35a055f5ef5785d51000638fdb2fb3aa2776
m6ttw <matthewrhyswilliams@live.com>
2021-03-02 14:48:42 +0000

    final commit

--- src/components/FizzBuzz.js
+++ src/components/FizzBuzz.js
@@ -6,14 +6,10 @@ const FizzBuzz = () => {
 
     const [output, setOutput] = useState("");
 
-    const handleIncrease = () => {
-        setNumber(number + 1)
-    }
-    const handleDecrease = () => {
-        setNumber(number - 1)
-    }
+    const [userAnswer, setUserAnswer] = useState("");
 
     useEffect(() => {
+        console.log("use effect triggered")
             if (number % 3 === 0 && number % 5 === 0) {
                 setOutput("fizzbuzz")
               } else if (number % 3 === 0) {
@@ -25,14 +21,41 @@ const FizzBuzz = () => {
               }
         }
     )
+
+    useEffect(() => {
+        console.log("user input triggered")
+            if (userAnswer == output) {
+                console.log("correct")
+            } else {
+                console.log("wrong")
+            }
+    })
+
+    const handleIncrease = () => {
+        setNumber(number + 1);
+    }
+    const handleDecrease = () => {
+        setNumber(number - 1);
+    }
+
+    const handleInput = (evt) => {
+        setNumber(evt.target.value);
+    }
+
+    const handleAnswer = (evt) => {
+        setUserAnswer(evt.target.value);
+    }
     
     return(
         <>
             <h2>FizzBuzz</h2>
             <h4>Current Number: {number}</h4>
-            <h5>Fizz / Buzz: {output}</h5>
+            <h5>Fizz / Buzz:</h5>
             <button onClick={handleIncrease}>Increase Number</button>
             <button onClick={handleDecrease}>Decrease Number</button>
+            <input type="number" placeholder="Your number here" value={number} onChange={handleInput}></input>
+            <input type="submit" onSubmit={handleInput}></input>
+            <input type="string" placeholder="Fizz or buzz?" value={userAnswer} onChange={handleAnswer}></input>
         </>
     )
 }
